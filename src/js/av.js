34eab63c81332510a4b96fd3b441d75f796f9513
shixiang <shixiangcool@163.com>
2018-06-17 23:55:43 +0800

    first commit

---
+++ src/js/av.js
@@ -0,0 +1,15 @@
+let APP_ID = 'gqfkWWD5iSM3WtC9EMlRHohd-gzGzoHsz'
+let APP_KEY = 'Ns87UicdeFTykmbPx4Q85P4t'
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
+
+// var TestObject = AV.Object.extend('TestObject');
+// var testObject = new TestObject();
+// testObject.save({
+//   words: 'Hello World!'
+// }).then(function(object) {
+//   alert('LeanCloud Rocks!');
+// })
