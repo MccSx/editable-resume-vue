716404b8d97181f299e9154d051b30ad9409cc9c
shixiang <shixiangcool@163.com>
2018-06-19 23:10:38 +0800

    完成登录注册功能

--- src/js/login.js
+++ src/js/login.js
@@ -1,16 +1,42 @@
 window.Login = {
+  data() {
+    return {
+      signUpData:{
+        email:'',
+        password:''
+      }
+    }
+  },
   template:`
   <div class="login-wrapper">
     <div class="login">
-      <form action="" class="form">
-        <input type="text" name="username">
-        <input type="password" name="password">
+      <button class="icon" @click="toIndex">X</button>
+      <form action="" class="form" @submit.prevent="onSignUp">
+        <h2>登录</h2>
+        <input type="text" name="email" placeholder="email" v-model="signUpData.email">
+        <input type="password" name="password" placeholder="password" v-model="signUpData.password">
         <button>登录</button>
-        <p><span>还没有账号？<a href="#">立即注册</a></span></p>
+        <p><span>还没有账号？<router-link to="/signUp">立即注册</router-link></span></p>
       </form>
     </div>
   </div>
-  `
+  `,
+  methods: {
+    toIndex() {
+      this.$router.push('/')
+    },
+    onSignUp() {
+      AV.User.logIn(this.signUpData.email, this.signUpData.password).then((user) => {
+        this.toIndex()
+      }, (error) => {
+        if (error.code === 211) {
+          alert('邮箱不存在')
+        } else if (error.code === 210) {
+          alert('邮箱和密码不匹配')
+        }
+      })
+    }
+  }
 }
 
 Vue.component('login',Login)
--- src/js/signUp.js
+++ src/js/signUp.js
@@ -1,16 +1,42 @@
 window.SignUp = {
+  data() {
+    return {
+      loginData:{
+        email:'',
+        password:''
+      }
+    }
+  },
   template:`
   <div class="signUp-wrapper">
     <div class="sign-up">
-      <form action="" class="form">
-        <input type="text" name="username">
-        <input type="password" name="password">
+      <button class="icon" @click="toIndex">X</button>
+      <form action="" class="form" @submit.prevent="onLogin">
+        <h2>注册</h2>
+        <input type="text" name="email" placeholder="email" v-model="loginData.email">
+        <input type="password" name="password" placeholder="password" v-model="loginData.password">
         <button>注册</button>
-        <p><span>已经有账号？<a href="#">立即登录</a></span></p>
+        <p><span>已经有账号？<router-link to="/login">立即登录</router-link></span></p>
       </form>
     </div>
   </div>
-  `
+  `,
+  methods: {
+    toIndex() {
+      this.$router.push('/')
+    },
+    onLogin() {
+      var user = new AV.User();
+      user.setUsername(this.loginData.email)
+      user.setPassword(this.loginData.password)
+      user.setEmail(this.loginData.email)
+      user.signUp().then((user) => {
+        alert('注册成功')
+      }, (error) => {
+        alert(error.rawMessage)
+      })
+    }
+  }
 }
 
 Vue.component('sign-up',SignUp)
